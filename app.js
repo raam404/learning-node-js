dcc290f0d45778deae1f7c813e392e54838c8b5c
unknown <rajurajbohara0123@gmail.com>
2026-06-20 17:08:19 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// console.log("hello world")
+// console.log(8*8)
+const a = 10;
+const b = 20;
+const sum = a + b
+console.log(sum)
+
+// array
+const numbers = [10, 20, 30, 40]
+console.log(numbers.length)
+console.log(numbers[0])
+
+// objects
+const person = {
+    name: "golu",
+    age: 15
+}
+console.log("This is my brother " + person.name)
+console.log(person.age)
+console.log(person)
